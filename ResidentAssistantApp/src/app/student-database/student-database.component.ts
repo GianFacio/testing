e3ebaff05e7d034fc1803f9f13b69560de26c6f1
Gian Facio <77515566+GianFacio@users.noreply.github.com>
2022-12-16 12:45:17 -0700

    Add files via upload

---
+++ ResidentAssistantApp/src/app/student-database/student-database.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student-database',
+  templateUrl: './student-database.component.html',
+  styleUrls: ['./student-database.component.css']
+})
+export class StudentDatabaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
